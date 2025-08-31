9cdb5f9b527da4bfe2881f10f7834110de934c5d
unaplanet <unaunaplanet5963@gmail.com>
2025-08-31 23:55:45 +0900

    ページをつくりました

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import router from './router' 
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/main.css';
 
-import './assets/main.css'
+const app = createApp(App);
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).use(router).mount('#app')
+app.use(router);
+app.mount('#app');
